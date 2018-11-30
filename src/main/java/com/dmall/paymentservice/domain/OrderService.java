5ba3150b2c3c8b1fc4572363fed4f3d06482d5c1
dqisme <dqisme@outlook.com>
2018-11-30 16:43:05 +0800

    [dq] #N/A fix order service for PUT method.

--- src/main/java/com/dmall/paymentservice/domain/OrderService.java
+++ src/main/java/com/dmall/paymentservice/domain/OrderService.java
@@ -1,11 +1,11 @@
 package com.dmall.paymentservice.domain;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @FeignClient(name = "order-service", url = "${order-service-url}")
 public interface OrderService {
-    @PatchMapping(value = "/orders/{orderId}", headers = {"Content-Type: application/json;charset=UTF-8"})
+    @PutMapping(value = "/orders/{orderId}", headers = {"Content-Type: application/json;charset=UTF-8"})
     public void updateOrderPaymentStatus(@PathVariable String orderId, OrderPaymentStatus orderPaymentStatus);
 }
